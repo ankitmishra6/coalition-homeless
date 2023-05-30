46cf8772e2204c334ab868ca1376b2019c4c6f12
Ankit Mishra <ankit.mishra@codemanas.com>
2023-05-30 12:57:53 +0545

    navbar done

--- dist/main.js
+++ dist/main.js
@@ -63,7 +63,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/style.scss */ "./src/scss/style.scss");
 
 
-
+// for header search icon
 let overlaySearchMenu = (function () {
     let DOM = {}
     function handleOverlayClose(e) {
@@ -88,15 +88,77 @@ let overlaySearchMenu = (function () {
         cacheDOM();
         eventListeners();
     }
-
     return {
         init
     }
-
 })();
 
-console.log(overlaySearchMenu)
-document.addEventListener('DOMContentLoaded', overlaySearchMenu.init)
+
+//for mobile navigation hamburger icon
+let mobileNavigation = (function () {
+    let DOM = {};
+
+    function menuActive(e) {
+        e.preventDefault();
+        DOM.mobNavOverlay.classList.toggle('active');
+    }
+    function eventListeners() {
+        DOM.hamburger.addEventListener('click', menuActive);
+    }
+    function cacheDOM() {
+        DOM.mobNavOverlay = document.querySelector('.mob-nav-menu');
+        DOM.hamburger = document.getElementById('icon');
+    }
+    // execute the mobmenu function
+    function init() {
+        cacheDOM();
+        eventListeners();
+    }
+    return {
+        init
+    }
+})();
+
+
+//for sub menu in mobile version
+let mobSubmenuDropdown = (function () {
+    let DOM = {};
+
+    function mobSubMenuOpen(e) {
+        e.preventDefault();
+        DOM.dropdownItems.style.display = "block";
+    }
+
+    function cacheDOM() {
+        DOM.dropdownItems = document.querySelector('.mob-sub-menu');
+        DOM.mobDropdown = document.querySelector('.mob-nav-links');
+    }
+    function eventListeners() {
+        DOM.mobDropdown.addEventListener('click', mobSubMenuOpen);
+    }
+    function init() {
+        cacheDOM();
+        eventListeners();
+    }
+    return {
+        init: init,
+    };
+})();
+
+
+
+
+console.log(overlaySearchMenu);
+document.addEventListener('DOMContentLoaded', overlaySearchMenu.init);
+
+console.log(mobileNavigation);
+document.addEventListener('DOMContentLoaded', mobileNavigation.init);
+
+console.log(mobSubmenuDropdown);
+document.addEventListener('DOMContentLoaded', mobSubmenuDropdown.init);
+
+
+
 })();
 
 /******/ })()
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,6 @@
 import '../scss/style.scss';
 
-
+// for header search icon
 let overlaySearchMenu = (function () {
     let DOM = {}
     function handleOverlayClose(e) {
@@ -25,12 +25,73 @@ let overlaySearchMenu = (function () {
         cacheDOM();
         eventListeners();
     }
-
     return {
         init
     }
-
 })();
 
-console.log(overlaySearchMenu)
-document.addEventListener('DOMContentLoaded', overlaySearchMenu.init)
+
+//for mobile navigation hamburger icon
+let mobileNavigation = (function () {
+    let DOM = {};
+
+    function menuActive(e) {
+        e.preventDefault();
+        DOM.mobNavOverlay.classList.toggle('active');
+    }
+    function eventListeners() {
+        DOM.hamburger.addEventListener('click', menuActive);
+    }
+    function cacheDOM() {
+        DOM.mobNavOverlay = document.querySelector('.mob-nav-menu');
+        DOM.hamburger = document.getElementById('icon');
+    }
+    // execute the mobmenu function
+    function init() {
+        cacheDOM();
+        eventListeners();
+    }
+    return {
+        init
+    }
+})();
+
+
+//for sub menu in mobile version
+let mobSubmenuDropdown = (function () {
+    let DOM = {};
+
+    function mobSubMenuOpen(e) {
+        e.preventDefault();
+        DOM.dropdownItems.style.display = "block";
+    }
+
+    function cacheDOM() {
+        DOM.dropdownItems = document.querySelector('.mob-sub-menu');
+        DOM.mobDropdown = document.querySelector('.mob-nav-links');
+    }
+    function eventListeners() {
+        DOM.mobDropdown.addEventListener('click', mobSubMenuOpen);
+    }
+    function init() {
+        cacheDOM();
+        eventListeners();
+    }
+    return {
+        init: init,
+    };
+})();
+
+
+
+
+console.log(overlaySearchMenu);
+document.addEventListener('DOMContentLoaded', overlaySearchMenu.init);
+
+console.log(mobileNavigation);
+document.addEventListener('DOMContentLoaded', mobileNavigation.init);
+
+console.log(mobSubmenuDropdown);
+document.addEventListener('DOMContentLoaded', mobSubmenuDropdown.init);
+
+
